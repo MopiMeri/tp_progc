790ee6510e139797c39b4768b6c1bc096f0c5b07
Dylan <106933517+MopiMeri@users.noreply.github.com>
2025-10-24 12:00:56 +0200

    Update pyramide.c

--- TP1/src/pyramide.c
+++ TP1/src/pyramide.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main() {
+    int n = 5; 
+
+    printf("=== Pyramide de nombres ===\n\n");
+
+    for (int i = 1; i <= n; i++) {
+        for (int espace = 1; espace <= n - i; espace++) {
+            printf(" ");
+        }
+
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+
+        for (int j = i - 1; j >= 1; j--) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    printf("\nPyramide terminée !\n");
+    return 0;
+}
