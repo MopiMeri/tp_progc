c0e2aaaf5fcfcda42f06fbbce47ed0dfd09049c6
Dylan <106933517+MopiMeri@users.noreply.github.com>
2025-10-24 11:41:30 +0200

    Add triangle rectangle pattern using nested loops

---
+++ TP1/src/boucles.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int compteur = 5;
+
+    printf("=== Triangle rectangle avec des boucles for ===\n\n");
+
+    for (int i = 1; i <= compteur; i++) {
+        for (int j = 1; j <= i; j++) {
+            if (i % 2 == 0 && j % 2 == 0)
+                printf("# ");
+            else
+                printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
