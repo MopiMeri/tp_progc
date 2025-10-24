8ee527ae18e23cc0dde74536006c42e49da4ed89
Dylan <106933517+MopiMeri@users.noreply.github.com>
2025-10-24 11:45:47 +0200

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -3,18 +3,22 @@
 int main() {
     int compteur = 8;
 
+    if (compteur >= 10) {
+        printf("Erreur : compteur doit être inférieur à 10.\n");
+        return 1;
+    }
+
     printf("=== Triangle rectangle avec des boucles for ===\n\n");
 
     for (int i = 1; i <= compteur; i++) {
         for (int j = 1; j <= i; j++) {
-            if (i % 2 == 0 && j % 2 == 0)
-                printf("# ");
-            else
+            if (j == 1 || j == i || i == compteur)
                 printf("* ");
+            else
+                printf("# ");
         }
         printf("\n");
     }
 
     return 0;
 }
-
